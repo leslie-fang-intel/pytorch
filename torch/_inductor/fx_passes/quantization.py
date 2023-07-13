10e3f5c7022c431cbec7a472aebc98c90a17ca1b
leslie-fang-intel <leslie.fang@intel.com>
2023-07-13 10:15:19 +0800

    enable quantization conv pattern fusion inside inductor
    ghstack-source-id: 84ec647e6fa792e75af0d0e2ef3fe5248f560873
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/104588

--- torch/_inductor/fx_passes/quantization.py
+++ torch/_inductor/fx_passes/quantization.py
@@ -1,7 +1,7 @@
 import functools
 
 import torch
-from ..ir import QConv
+from ..ir import QConvPointWisePT2E, TensorBox
 from ..pattern_matcher import Arg, CallFunction, KeywordArg, Match
 from .freezing_patterns import register_freezing_graph_pattern
 from .post_grad import register_lowering_pattern
@@ -9,7 +9,6 @@ from .post_grad import register_lowering_pattern
 aten = torch.ops.aten
 prims = torch.ops.prims
 quantized_decomposed = torch.ops.quantized_decomposed
-dequantize_per_channel = quantized_decomposed.dequantize_per_channel.default
 
 """
 dequantize activation:
@@ -17,7 +16,7 @@ dequantize activation:
     x = x - zero_point
     x = x * scale
 """
-dequantize_activation_pattern = CallFunction(
+dequantize_per_tensor_activation_pattern = CallFunction(
     aten.mul.Tensor,
     CallFunction(
         aten.sub.Tensor,
@@ -31,28 +30,43 @@ dequantize_activation_pattern = CallFunction(
     KeywordArg("x_scale"),
 )
 
-dequantize_weight_pattern = CallFunction(
-    dequantize_per_channel,
-    KeywordArg("w"),
+dequantize_per_channel_weight_pattern = CallFunction(
+    quantized_decomposed.dequantize_per_channel.default,
+    KeywordArg("q_weight"),
     KeywordArg("w_scale"),
     KeywordArg("w_zp"),
-    KeywordArg("w_axis"),  # axis for quantization
-    KeywordArg("w_qmin"),  # quant clamp min
-    KeywordArg("w_qmax"),  # quant clamp max
-    KeywordArg("qw_dtype"),  # dtype=torch.int8
+    KeywordArg("w_axis"),
+    KeywordArg("w_quant_min"),
+    KeywordArg("w_quant_max"),
+    KeywordArg("w_dtype"),
 )
 
-aten_conv_pattern = CallFunction(
-    aten.convolution.default,
-    dequantize_activation_pattern,
-    dequantize_weight_pattern,
+
+dequantize_per_channel_clone_weight_pattern = CallFunction(
+    aten.clone.default,
+    dequantize_per_channel_weight_pattern,
+    memory_format=KeywordArg("memory_format"),
+)
+
+dequantize_qconv_pt2e_pattern = CallFunction(
+    torch.ops.onednn.qconv2d_pointwise.default,
+    KeywordArg("x"),
+    KeywordArg("x_scale"),  # x_scale
+    KeywordArg("x_zp"),  # x_zp
+    KeywordArg("packed_weight"),  # packed_weight
+    KeywordArg("w_scale"),  # w_scale
+    KeywordArg("w_zp"),  # w_zp
     KeywordArg("b"),  # bias
     KeywordArg("stride"),
     KeywordArg("padding"),
     KeywordArg("dilation"),
-    KeywordArg("transposed"),
-    KeywordArg("o_padding"),
     KeywordArg("groups"),
+    KeywordArg("output_scale"),  # output_scale
+    KeywordArg("output_zero_point"),  # output_zero_point
+    KeywordArg("fp32_output"),  # fp32_output
+    KeywordArg("attr"),  # attr
+    Arg(),  # scalars
+    Arg(),  # algorithm
 )
 
 """
@@ -65,7 +79,7 @@ quantize output:
     output = clamp_max(output, 127)
     output = output.to(uint8)
 """
-quantize_conv_output_pattern = CallFunction(
+quantize_conv_output_pattern_pt2e = CallFunction(
     prims.convert_element_type.default,
     CallFunction(
         aten.clamp_max.default,
@@ -77,14 +91,8 @@ quantize_conv_output_pattern = CallFunction(
                     aten.round.default,
                     CallFunction(
                         aten.mul.Tensor,
-                        aten_conv_pattern,  # output of conv
-                        CallFunction(
-                            aten.mul.Tensor,
-                            CallFunction(
-                                aten.reciprocal.default, KeywordArg("o_scale")
-                            ),
-                            Arg(),  # 1.0
-                        ),
+                        dequantize_qconv_pt2e_pattern,
+                        KeywordArg("o_inv_scale"),
                     ),
                 ),
                 KeywordArg("o_zp"),
@@ -96,74 +104,77 @@ quantize_conv_output_pattern = CallFunction(
     KeywordArg("o_dtype"),  # dtype=torch.uint8
 )
 
-
-dequant_per_channel_pattern = CallFunction(
-    quantized_decomposed.dequantize_per_channel.default,  # dequant_per_channel node
-    KeywordArg("q_weight"),
-    KeywordArg("w_scale"),
-    KeywordArg("w_zp"),
-    KeywordArg("w_axis"),
-    KeywordArg("w_quant_min"),
-    KeywordArg("w_quant_max"),
-    KeywordArg("w_dtype"),
-)
-
-
-dequant_per_channel_clone_to_channel_last_pattern = CallFunction(
-    aten.clone.default,
-    dequant_per_channel_pattern,
-    memory_format=KeywordArg("memory_format"),
-)
+pattern_match_count = 0
 
 
 def _register_quantized_conv_lowering(pattern):
     @register_lowering_pattern(pattern)
     def qconv(match: Match, *args, **kwargs):
         x, x_scale, x_zp = kwargs["x"], kwargs["x_scale"], kwargs["x_zp"]
-        w, w_scale, w_zp, w_axis = (
-            kwargs["w"],
-            kwargs["w_scale"],
-            kwargs["w_zp"],
-            kwargs["w_axis"],
-        )
         b, stride, padding, dilation = (
             kwargs["b"],
             kwargs["stride"],
             kwargs["padding"],
             kwargs["dilation"],
         )
-        groups, o_scale, o_zero_point, o_dtype = (
+        groups, o_inv_scale, o_zero_point, o_dtype = (
             kwargs["groups"],
-            kwargs["o_scale"],
+            kwargs["o_inv_scale"],
             kwargs["o_zp"],
             kwargs["o_dtype"],
         )
-        weight_shape = w.get_size()
+
+        # packed_weight = kwargs["packed_weight"]
+        packed_weight, w_scale, w_zp = (
+            kwargs["packed_weight"],
+            kwargs["w_scale"],
+            kwargs["w_zp"],
+        )
+        global pattern_match_count
+        pattern_match_count += 1
+        print(
+            "---- matched the pattern ----: {}".format(pattern_match_count), flush=True
+        )
+
+        assert (
+            kwargs["fp32_output"] is True
+        )  # Expected int8-in fp32-out qconv in weight prepack phase
+        assert (
+            kwargs["attr"] == "none"
+        )  # Expected no post op fused in weight prepack phase
+
+        weight_shape = packed_weight.get_size()
         dim = len(weight_shape) - 2
-        return QConv.create(
-            dim,
-            x,
-            x_scale,
-            x_zp,
-            w,
-            w_scale,
-            w_zp,
-            w_axis,
-            b,
-            stride,
-            padding,
-            dilation,
-            groups,
-            o_scale,
-            o_zero_point,
-            o_dtype,
+        return TensorBox.create(
+            QConvPointWisePT2E.create(
+                dim,
+                x,
+                x_scale,
+                x_zp,
+                packed_weight,
+                w_scale,
+                w_zp,
+                -1,  # w_axis delete it later
+                b,
+                stride,
+                padding,
+                dilation,
+                groups,
+                o_inv_scale,
+                o_zero_point,
+                o_dtype,
+                False,  # fp32_output
+                "none",  # unary_attr
+                [],  # unary_scalars
+                "",  # unary_algorithm
+            )
         )
 
     return qconv
 
 
 def register_quantization_lowerings():
-    _register_quantized_conv_lowering(quantize_conv_output_pattern)
+    _register_quantized_conv_lowering(quantize_conv_output_pattern_pt2e)
 
 
 def _is_valid_dequant_conv2d_pattern(match):
@@ -326,7 +337,7 @@ def _register_qconv_weight_prepack_pass(pattern):
 def _generate_dequant_convolution_node_pattern(_dequant_per_channel_pattern):
     dequant_convolution_node_pattern = CallFunction(
         aten.convolution.default,
-        dequantize_activation_pattern,
+        dequantize_per_tensor_activation_pattern,
         _dequant_per_channel_pattern,
         KeywordArg("b"),
         KeywordArg("stride"),
@@ -341,13 +352,15 @@ def _generate_dequant_convolution_node_pattern(_dequant_per_channel_pattern):
 
 def _generate_qconv_weight_prepack_patterns():
     return (
-        _generate_dequant_convolution_node_pattern(dequant_per_channel_pattern),
+        _generate_dequant_convolution_node_pattern(
+            dequantize_per_channel_weight_pattern
+        ),
         # There is another pattern due to the pass of convert_conv_weights_to_channels_last
         # https://github.com/pytorch/pytorch/blob/07107919297db3f8ab37f11c12666b6d6d5f692e/torch/_inductor/freezing.py#L338-L362.
         # Depend on some heuristics, it may or may not insert to(channel_last) node
         # between convolution and dequant_per_channel node
         _generate_dequant_convolution_node_pattern(
-            dequant_per_channel_clone_to_channel_last_pattern
+            dequantize_per_channel_clone_weight_pattern
         ),
     )
 
