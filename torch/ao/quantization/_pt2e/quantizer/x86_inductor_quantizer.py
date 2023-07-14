026f7d20ab64367fbdaaa34802310c1f18d0078a
leslie-fang-intel <leslie.fang@intel.com>
2023-07-15 06:36:11 +0800

    single QConv int8 in fp32 out

--- torch/ao/quantization/_pt2e/quantizer/x86_inductor_quantizer.py
+++ torch/ao/quantization/_pt2e/quantizer/x86_inductor_quantizer.py
@@ -186,7 +186,7 @@ class X86InductorQuantizer(Quantizer):
             conv_node.meta["quantization_annotation"] = QuantizationAnnotation(
                 input_qspec_map=input_qspec_map,
                 # TODO<leslie> Remove the annotate of output when oneDNN qconv support fp32 out.
-                output_qspec=get_output_act_qspec(quantization_config),
+                # output_qspec=get_output_act_qspec(quantization_config),
                 _annotated=True
             )
         else:
