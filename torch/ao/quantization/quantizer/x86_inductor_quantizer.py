e880cd326937fa74482e7235a05158dc50069e3a
leslie-fang-intel <leslie.fang@intel.com>
2023-10-24 10:11:51 +0800

    poc error

--- torch/ao/quantization/quantizer/x86_inductor_quantizer.py
+++ torch/ao/quantization/quantizer/x86_inductor_quantizer.py
@@ -68,6 +68,25 @@ int8_in_int8_out_ops_pt2e: Set = {
     torch.ops.aten.avg_pool2d.default,
 }
 
+from torch.fx.passes.utils.matcher_with_name_node_map_utils import SubgraphMatcherWithNameNodeMap
+from torch._export import capture_pre_autograd_graph
+
+_quantized_add_pattern_example_inputs = (
+    torch.randn(1, 1, 3, 3),  # input1
+    torch.randn(1, 1, 1, 1),  # weight1
+    torch.randn(1),           # bias1
+    torch.randn(1, 1, 3, 3),  # input2
+    torch.randn(1, 1, 1, 1),  # weight2
+    torch.randn(1),           # bias2
+)
+
+def conv_add_pattern(input1, weight1, bias1, input2, weight2, bias2):
+    conv1 = torch.nn.functional.conv2d(input1, weight1, bias1)
+    conv2 = torch.nn.functional.conv2d(input2, weight2, bias2)
+    output = conv1 + conv2
+    return output, {"input1": input1, "weight1": weight1, "bias1": bias1, "input2": input2, "weight2": weight2, "bias2": bias2, "output": output}
+
+conv_add_pattern_gm = capture_pre_autograd_graph(conv_add_pattern, _quantized_add_pattern_example_inputs)
 
 QUANT_ANNOTATION_KEY = "quantization_annotation"
 
